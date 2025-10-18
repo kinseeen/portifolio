1b1c5c69117da6557a5aea1ff04465d7aea66b19
kinseeen <kine.jakobsen@hotmail.no>
2025-10-18 20:22:30 +0200

    Update EcomStore.jsx

--- my-app/src/pages/EcomStore.jsx
+++ my-app/src/pages/EcomStore.jsx
@@ -6,7 +6,7 @@ export default function EcomStore() {
     <ArticlePageItem
       title="Ecom Store"
       image="/images/ecomstorefront.png"
-      mainContent="This website was created for my javascript frameworks assignment. This project was created using React and bootstrap. 
+      mainContent="This website was created for my javascript frameworks assignment. This project was created using React.
       The aim of this assignment was to create an e-commerce store, and collect data using Noroff API. The website was required to have a homepage, individual product page, cart page, checkout success page and contact page. Please look at the github readme for this page to read more about this project! "
       liveSite="https://ecommercekine.netlify.app/"
       github="https://github.com/kinseeen/ecom-store/blob/master/README.md"
